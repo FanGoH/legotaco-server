14e71b3d9d350dc887ef04490d77ed3fde8c4956
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2021-09-25 12:57:00 -0700

    Updating Main

--- __main__.py
+++ __main__.py
@@ -1,1 +1,17 @@
+from queue import SimpleQueue
+
+class PriorityQueue:
+ def __init__(self):
+    self.newOrder =SimpleQueue()
+    self.PreviosulyWorkedOrders=SimpleQueue()
+    self.PausedOrder=SimpleQueue()
+
+def RoundRobin():
+    pass
+    #implement Round Robin
+
+def Taqueria():
+    pass
+
+
 print("Hello World")
