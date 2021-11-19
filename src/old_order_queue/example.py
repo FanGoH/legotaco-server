f3fb41cb507191b43bf477021e63efdbff5646bc
despelair <52083283+DESPEL@users.noreply.github.com>
2021-11-18 18:29:06 -0800

    renamed order_queue to old_order_queue

--- src/old_order_queue/example.py
+++ src/old_order_queue/example.py
@@ -3,6 +3,7 @@ import copy
 from datetime import datetime
 import simplejson as json
 
+
 def generate_tacos():
     tacos = []
     type = ["taco", "quesadilla"]
@@ -10,20 +11,20 @@ def generate_tacos():
     fillings = ["cebolla", "cilantro", "salsa", "guacamole"]
     for x in range(random.randrange(100)):
         taco = {
-            "datetime": str(datetime.now()), 
-            "request_id": x, 
-            "status": "open", 
-            "orden":[ ] 
+            "datetime": str(datetime.now()),
+            "request_id": x,
+            "status": "open",
+            "orden": []
         }
         for y in range(random.randrange(10)):
             taco["orden"].append(
-                {   
-                    "part_id": "{0}-{1}".format(x, y), 
-                    "type": random.choice(type), 
-                    "meat": random.choice(meat), 
-                    "status":"open", 
-                    "quantity": random.randrange(100), 
-                    "ingredients":[ ] 
+                {
+                    "part_id": "{0}-{1}".format(x, y),
+                    "type": random.choice(type),
+                    "meat": random.choice(meat),
+                    "status": "open",
+                    "quantity": random.randrange(100),
+                    "ingredients": []
                 }
             )
             local_fillings = copy.deepcopy(fillings)
@@ -32,7 +33,7 @@ def generate_tacos():
                 taco["orden"][y]["ingredients"].append(ind_filling)
                 local_fillings.remove(ind_filling)
 
-        tacos.append(taco)  
+        tacos.append(taco)
 
     tacos_file = open("tacos.json", "w")
     tacos_file.write(json.dumps(tacos))
@@ -40,4 +41,3 @@ def generate_tacos():
 
 
 generate_tacos()
-
