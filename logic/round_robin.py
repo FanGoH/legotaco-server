76b066de19b86b5683f581bc652a7fe185e0970a
despelair <52083283+DESPEL@users.noreply.github.com>
2021-11-21 19:10:14 -0800

    started parallelizing round_robin

--- logic/round_robin.py
+++ logic/round_robin.py
@@ -1,14 +1,26 @@
+import threading
+
 class RoundRobin:
     i = 0
     elements = []
+    working = {}
 
-    def __init__(self, elements: list):
+    def __init__(self, elements: list, job_replacer: function[int], lock: threading.Lock):
         self.elements = elements
+        self.replacer = job_replacer
+        self.lock = lock
 
-    def next_job(self):
-        self.i = (self.i + 1) % len(self.elements)
-        return self.elements[self.i-1]
+    def work_on_next(self, worker):
+        # an instance of RoundRobin can be used buy multiple Workers at the same time
+        # it should not be possible to work on the same job at the same time
+        # tracking working jobs is necesary
+            # we don't want to pollute the calling scope
+            # our option is to use a callback function that works over the object
+        self.lock.acquire()
+        
+        self.lock.release()
+        return
 
-    def complete_job(self, replacer):
-        self.elements[self.i] = replacer
+    def complete_job(self):
+        self.elements[self.i] = self.replacer(self.i)
 
