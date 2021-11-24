07cec026b8a2099c474c9753a4143c8f6a94af6b
despelair <52083283+DESPEL@users.noreply.github.com>
2021-11-23 18:48:07 -0800

    The filling can now be initialized with a different amount available

--- logic/filling.py
+++ logic/filling.py
@@ -1,6 +1,9 @@
 class Filling:
 
-    def __init__(self, max_):
+    def __init__(self, max_, available=None):
+        if available is None:
+            available = max_
+
         self.max = max_
-        self.available = max_
+        self.available = available
         pass
