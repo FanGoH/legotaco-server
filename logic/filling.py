2f37d0da6a8f018520d81734643d2759351d837c
despelair <52083283+DESPEL@users.noreply.github.com>
2021-11-23 18:59:30 -0800

    filling now has a name

--- logic/filling.py
+++ logic/filling.py
@@ -1,9 +1,10 @@
 class Filling:
 
-    def __init__(self, max_, available=None):
+    def __init__(self, max_, available=None, name="Default Filling"):
         if available is None:
             available = max_
 
         self.max = max_
         self.available = available
+        self.name = name
         pass
