7d0273f48dfc024ae254c055fc87a5bb0838ee13
despelair <52083283+DESPEL@users.noreply.github.com>
2021-11-28 23:31:06 -0800

    Generated simple dataclasses to improve VS Code linting

---
+++ logic/data_classes.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from typing import List
+from logic.order import Order
+from logic.order_queue import OrderQueue
+from logic.round_robin import Scheduler
+
+from logic.taquero import Taquero, TaqueroConfig
+from logic.filling import Filling
+
+@dataclass
+class GenericTaquero:
+    taquero: Taquero
+    fillings: List[Filling]
+    quesadillas: Filling
+    config: TaqueroConfig
+
+@dataclass
+class GeneratedScheduler:
+    scheduler: Scheduler
+    elements: List[Order]
+    queue: OrderQueue
+
