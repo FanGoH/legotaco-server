10cb7454b3ada057f64d7073d434ced204d01b43
ManmadeArc <44855389+ManmadeArc@users.noreply.github.com>
2021-12-05 23:02:37 -0800

    asdasd

--- subirAqueue.py
+++ subirAqueue.py
@@ -1,6 +1,7 @@
 import boto3
 import json
 from time import sleep
+import example
 
 sqs = boto3.client('sqs')
 
@@ -8,7 +9,8 @@ load = open('tacos.json', 'r')
 
 data = json.load(load)
 
-for _ in range(50):
+while True:
+    example.generate_tacos()
     for i in data:
         print("UWU")
         sqs.send_message(QueueUrl = 'https://sqs.us-east-1.amazonaws.com/292274580527/sqs_cc106_team_1', MessageBody = json.dumps(i))   
